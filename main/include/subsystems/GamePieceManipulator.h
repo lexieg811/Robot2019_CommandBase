b531ee70c749c71d845d3312a54d2008ab36ca81
Rob Pellowski <23526472+RPellowski@users.noreply.github.com>
2019-02-19 17:51:26 -0500

    Hatch PID and other tweaks (#30)
    * Minor tweaks for hatch PID
    
    * More hatch PID tweaks
    
    * tweeked manual hatch
    
    * Robot.cpp changes
    
    * Undo silliness

--- main/include/subsystems/GamePieceManipulator.h
+++ main/include/subsystems/GamePieceManipulator.h
@@ -16,8 +16,8 @@
 #include <frc/AnalogInput.h>
 
 // Max and min voltage for hinge pot positions
-#define HINGE_MAX_LEFT 4.7
-#define HINGE_MIN_LEFT .7
+#define HINGE_MAX_LEFT 4.3
+#define HINGE_MIN_LEFT .45
 #define HINGE_MAX_RIGHT 4.95
 #define HINGE_MIN_RIGHT 0.3
 // Top: 4.7V Bottom: 1.2V (3.5V = 90 degrees)
